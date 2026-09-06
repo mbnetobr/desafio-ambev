f2d670b59d5d28f561bfe60e4b266368ebfda60c
Manuel Neto <mbn.man@gmail.com>
2026-09-06 14:12:28 -0400

    ci: correcao de arquivo de com metodos do serviço de produtos nao enviado nos testes anteriores

--- cypress/services/ProductService.js
+++ cypress/services/ProductService.js
@@ -3,8 +3,8 @@ import { faker } from '@faker-js/faker';
 class ProductService {
   gerarDadosProduto() {
     return {
-      nome: `Produto ${faker.commerce.productName()} ${faker.string.alphanumeric(5)}`,
-      preco: parseInt(faker.commerce.price({ min: 10, max: 500 })),
+      nome: faker.commerce.productName() + ` ${Date.now()}`,
+      preco: Number(faker.commerce.price({ min: 10, max: 1000 })),
       descricao: faker.commerce.productDescription(),
       quantidade: faker.number.int({ min: 1, max: 100 })
     };
@@ -14,10 +14,43 @@ class ProductService {
     return cy.request({
       method: 'POST',
       url: `${Cypress.env('apiUrl')}/produtos`,
-      headers: {
-        authorization: token
-      },
       body: produto,
+      headers: { authorization: token },
+      failOnStatusCode: false
+    });
+  }
+
+  listarProdutos() {
+    return cy.request({
+      method: 'GET',
+      url: `${Cypress.env('apiUrl')}/produtos`,
+      failOnStatusCode: false
+    });
+  }
+
+  buscarProdutoPorId(id) {
+    return cy.request({
+      method: 'GET',
+      url: `${Cypress.env('apiUrl')}/produtos/${id}`,
+      failOnStatusCode: false
+    });
+  }
+
+  deletarProduto(id, token) {
+    return cy.request({
+      method: 'DELETE',
+      url: `${Cypress.env('apiUrl')}/produtos/${id}`,
+      headers: { authorization: token },
+      failOnStatusCode: false
+    });
+  }
+
+  editarProduto(id, produto, token) {
+    return cy.request({
+      method: 'PUT',
+      url: `${Cypress.env('apiUrl')}/produtos/${id}`,
+      body: produto,
+      headers: { authorization: token },
       failOnStatusCode: false
     });
   }
